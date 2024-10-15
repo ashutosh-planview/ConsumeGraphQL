e9d2b48a05d101b64fa1b943d97e0fe32b331657
Kumar Ashutosh <kumar.ashutosh@planview.com>
2024-10-15 12:30:43 +0530

    Project to consume GraphQL Get-endpoints using libraries and packages

---
+++ WithLibConsumeGQL/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using DTOs;
+using GraphQL;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+
+namespace WithLibConsumeGQL
+{
+  internal static class Program
+  {
+    public static async Task Main()
+    {
+      Console.WriteLine("Hello, World!");
+
+      // The GraphQL endpoint URL
+      const string endpointGQLUrl = "http://localhost:8081/v1/graphql";
+
+      // add authorization token here without prepending 'Bearer ' to it.
+      const string tokenBearer =
+        "";
+
+      var httpClient = new HttpClient();
+      httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenBearer);gfh  jghjfghjhggwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+
+      var graphQLClient = new GraphQLHttpClient(endpointGQLUrl, new NewtonsoftJsonSerializer(), httpClient);
+
+      var request = new GraphQLRequest
+      {
+        Query = @"
+          query GetCurrentUser {
+            pvadmin_ {
+              getCurrentUser {
+                id
+                firstName
+                lastName
+                email
+                role
+              }
+            }
+          }
+        "
+      };
+
+      try
+      {
+        var response = await graphQLClient.SendQueryAsync<PvadminDto<GetCurrentUserDto>>(request).ConfigureAwait(false);
+        
+        if (response.Errors == null || response.Errors.Length == 0)
+        {
+          var currentUser = response.Data.Pvadmin_.GetCurrentUser;
+          Console.WriteLine("Current User: \n" + currentUser);
+        }
+        else
+        {
+          Console.WriteLine("Something went wrong: \n" + response.Errors);
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine("Some exception \n" + e.Message);
+      }
+    }
+  }
+}
---
+++ WithLibConsumeGQL/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WithLibConsumeGQL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WithLibConsumeGQL")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("99E921A4-B6C5-4194-A97F-B1CDD0690FE7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
